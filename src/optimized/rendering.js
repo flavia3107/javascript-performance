6ca05ab16caaf7cb5cb1f85a317fb4effbe52458
Flavia Kosti <flaviakosti@gmail.com>
2024-11-28 15:50:18 -0500

    Performance & Optimization: Re-design P3

--- src/optimized/rendering.js
+++ src/optimized/rendering.js
@@ -3,12 +3,12 @@ const productListEl = document.getElementById('product-list');
 function createElement(product, prodId, deleteProductFn) {
   const newListEl = document.createElement('li');
   newListEl.innerHTML = `
-    <h2>${product.title}</h2>
-    <p>${product.price}</p>
+    <span class="subtitle">Product</span>
+    <h3>${product.title}</h3>
+    <p class="price_container">Price: <span class="price">$${parseFloat(product.price).toFixed(2)}</span></p>
   `;
   const prodDeleteButtonEl = document.createElement('button');
-  prodDeleteButtonEl.textContent = 'DELETE';
-
+  prodDeleteButtonEl.innerHTML = '<i class="fa fa-trash-o"></i>';
   newListEl.id = prodId;
 
   prodDeleteButtonEl.addEventListener(
@@ -16,8 +16,7 @@ function createElement(product, prodId, deleteProductFn) {
     deleteProductFn.bind(null, prodId)
   );
 
-  newListEl.appendChild(prodDeleteButtonEl);
-
+  newListEl.prepend(prodDeleteButtonEl);
   return newListEl;
 }
 
@@ -36,14 +35,12 @@ export function addFormListItem() {
   newListEl.innerHTML = `
   <form id="new-product">
     <div class="form-control">
-      <label for="title">Title</label>
-      <input type="text" id="title" />
+      <input type="text" id="title" placeholder="Title" minlength="4" />
     </div>
     <div class="form-control">
-      <label for="price">Price</label>
-      <input type="number" min="0" step="0.01" id="price" />
+      <input type="number" min="0" step="0.01" id="price" placeholder="Price" />
     </div>
-    <button type="submit">ADD PRODUCT</button>
+    <button type="submit"> + Add</button>
   </form>
   `;
   productListEl.appendChild(newListEl);
